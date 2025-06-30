05733d08049659eddb49942820600b644f32eec6
alpacaman10 <iambrent90@gmail.com>
2025-06-29 21:24:27 -0500

    few more small thigns

--- PC_navigator_home.py
+++ PC_navigator_home.py
@@ -1,4 +1,7 @@
 import streamlit as st
 
-pg = st.navigation([st.Page("pages/app_pilot_m22.py"), st.Page("pages/app_pilot_10250T.py")])
+pg = st.navigation([
+    st.Page("pages/app_pilot_m22.py"), 
+    st.Page("pages/app_pilot_10250T.py")
+    ])
 pg.run()
